afc4d2e189616fa7bc242c38e063c60b143ae732
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-10-25 11:04:15 -0300

    create functions for mutations

--- init-db.sql
+++ init-db.sql
@@ -26,3 +26,28 @@ CREATE TABLE IF NOT EXISTS comments (
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (post_id) REFERENCES posts (id)
 );
+
+CREATE FUNCTION create_user(
+    username VARCHAR(60),
+    password VARCHAR(80),
+    email TEXT
+) RETURNS users AS $$ 
+    INSERT INTO users (username, password, email) VALUES ($1, crypt($2, gen_salt('bf')), $3) RETURNING *;
+$$ LANGUAGE SQL;
+
+CREATE FUNCTION create_user_with_first_post(
+    username VARCHAR(60),
+    password VARCHAR(80),
+    email TEXT,
+    post_title VARCHAR(100),
+    post_body TEXT
+) RETURNS users AS $$
+BEGIN
+
+    DECLARE new_user users;
+    new_user := create_user(username, password, email);
+    INSERT INTO posts (title, body, user_id) VALUES (post_title, post_body, new_user.id);
+    RETURN new_user;
+
+END;
+$$ LANGUAGE plpgsql;
